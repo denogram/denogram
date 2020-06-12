26e0c08e15f569938b3669d87b92876cf1122029
Stanislav Strelnikov <stanislav.strelnikov@icloud.com>
2020-06-13 00:44:56 +0300

    feat(context): state

--- context.ts
+++ context.ts
@@ -1,29 +1,44 @@
 // Copyright 2020 the denogram authors. All rights reserved. MIT license.
 import { Telegram } from "./telegram.ts";
-import { Update, Message, Chat } from "./_types/mod.ts";
+import { Update, Chat, Message } from "./_types/mod.ts";
+import { SendMessageParams } from "./_types/params/mod.ts";
+
+export type ReplyParams = Omit<
+  SendMessageParams,
+  "chat_id" | "text" | "reply_to_message_id"
+>;
 
 /** Context */
 export class Context {
+  public readonly state = new Map<string, unknown>();
+
   public readonly message?: Message;
   public readonly chat?: Chat;
 
   constructor(
-    public readonly update: Update,
+    private readonly _update: Update,
     public readonly telegram: Telegram,
   ) {
-    this.message = update.message;
+    this.message = _update.message;
     this.chat = this.message?.chat;
   }
 
-  public async reply(text: string): Promise<void> {
+  public reply(
+    text: string,
+    params?: ReplyParams,
+  ): Promise<Message> | undefined {
     if (this.message !== undefined && this.chat !== undefined) {
-      await this.telegram.sendMessage({
-        chatId: this.chat.id,
+      return this.telegram.sendMessage(this.chat.id, {
         text,
-        params: {
-          reply_to_message_id: this.message.message_id,
-        },
+        reply_to_message_id: this.message.message_id,
+        ...params,
       });
+    }
+  }
+
+  public deleteMessage(): Promise<boolean> | undefined {
+    if (this.message !== undefined && this.chat !== undefined) {
+      return this.telegram.deleteMessage(this.chat.id, this.message.message_id);
     }
   }
 }
